7d03fa410ff4541682d1cf50e58f234c237a188d
melsonGit <50531920+melsonGit@users.noreply.github.com>
2021-07-26 20:52:48 +0100

    Update 2d_convolution_serial_final.cpp

--- final_project_code/serial/2d_convolution_serial_final/2d_convolution_serial_final.cpp
+++ final_project_code/serial/2d_convolution_serial_final/2d_convolution_serial_final.cpp
@@ -2,7 +2,7 @@
 //
 // Code sourced and adpated from the following author/s and sources: 
 // - https://www.youtube.com/watch?v=qxcfco89wvs
-// - https://github.com/CoffeeBeforeArch/cuda_programming/blob/master/convolution/2d_constant_memory/convolution.cu
+// - https://github.com/CoffeeBeforeArch/cuda_programming/blob/6589c89a78dee44e14ccb362cdae69f2e6850a2c/convolution/2d_constant_memory/convolution.cu
 // - https://mathworld.wolfram.com/Convolution.html
 // Please refer to the bibliography for a complete reference of the above author/s and sources
 
@@ -20,31 +20,32 @@ using std::vector;
 // Amount the the matrix will hang over the matrix
 #define MASK_OFFSET (MASK_DIM / 2)
 
-// Allocate mask in constant memory
-int mask[7 * 7];
+
+
 
 // Function Prototypes
 int element_set(int);
 void init_matrix(vector<int>, int);
 void convolution_2d(vector<int>, vector<int>, int);
 
-int main() {
+int main()
+{
 
 
     // Call element_set function to assign variable no_elements with a user selected value || Sets number of elements to be used
     static int no_elements = element_set(no_elements);
 
+    // Allocate the matrix with no_elements
+    vector<int> main_vector(no_elements * no_elements);
+    // Allocate the mask with MASK_DIM
+    vector<int> h_mask(MASK_DIM * MASK_DIM);
+
     clock_t start = clock();
 
-    // Allocate the matrix and initialise it
-    vector<int> matrix(no_elements * no_elements);
-    init_matrix(matrix, no_elements);
-
-    // Allocate the mask and initialise it
-    vector<int> h_mask (MASK_DIM * MASK_DIM);
+    init_matrix(main_vector, no_elements);
     init_matrix(h_mask, MASK_DIM);
 
-    convolution_2d(matrix, h_mask, no_elements);
+    convolution_2d(main_vector, h_mask, no_elements);
 
     clock_t end = clock();
 
@@ -55,7 +56,8 @@ int main() {
     return EXIT_SUCCESS;
 }
 
-int element_set(int element_size) {
+int element_set(int element_size)
+{
 
     int temp_input;
 
@@ -72,20 +74,25 @@ int element_set(int element_size) {
         cout << "\n\nNo correct option selected!\nShutting down program....\n";
         return EXIT_FAILURE;
     }
-        // 4096 elements
-    if (temp_input == 1) {
+    // 4096 elements
+    if (temp_input == 1)
+    {
         element_size = 4096;
     }   // 5120 elements
-    else if (temp_input == 2) {
+    else if (temp_input == 2)
+    {
         element_size = 5120;
     }   // 6144 elements
-    else if (temp_input == 3) {
+    else if (temp_input == 3)
+    {
         element_size = 6144;
     }   // 8192 elements
-    else if (temp_input == 4) {
+    else if (temp_input == 4)
+    {
         element_size = 8192;
     }   // 10240 elements
-    else if (temp_input == 5) {
+    else if (temp_input == 5)
+    {
         element_size = 10240;
     }
 
@@ -93,60 +100,68 @@ int element_set(int element_size) {
 }
 
 
-//Function Declarations
-
-// Initialises an no_elements x no_elements matrix with random numbers
-// Arguments:
-//  m = Pointer to the matrix
-//  no_elements = Dimension of the matrix (square)
-void init_matrix(vector<int> m, int no_elements) {
-    for (int i = 0; i < no_elements; i++) {
-        for (int j = 0; j < no_elements; j++) {
-            m[no_elements * i + j] = rand() % 100;
-        }
-    }
-}
-
 // Executes 2D convolution
 // Arguments:
-//  m = Original matrix
+//  main_vector = Original matrix
 //  mask = Convolutional mask
 //  no_elements = Dimensions of the matrix
 
-void convolution_2d(vector<int> m, vector<int> mask, int no_elements) {
-    // Temp value for accumulating results
-    int temp;
+void convolution_2d(vector<int> main_vector, vector<int> mask, int no_elements)
+{
+    // Variable for accumulating results
+    int convo_output;
 
     // Intermediate value for more readable code
     int offset_r;
     int offset_c;
 
     // Go over each row
-    for (int i = 0; i < no_elements; i++) {
+    for (int i = 0; i < no_elements; i++)
+    {
         // Go over each column
-        for (int j = 0; j < no_elements; j++) {
-            // Assign the temp variable a value
-            temp = 0;
+        for (int j = 0; j < no_elements; j++)
+        {
+            // Assign the convo_output variable a value
+            convo_output = 0;
 
             // Go over each mask row
-            for (int k = 0; k < MASK_DIM; k++) {
+            for (int k = 0; k < MASK_DIM; k++)
+            {
                 // Update offset value for row
                 offset_r = i - MASK_OFFSET + k;
 
                 // Go over each mask column
-                for (int l = 0; l < MASK_DIM; l++) {
+                for (int l = 0; l < MASK_DIM; l++)
+                {
                     // Update offset value for column
                     offset_c = j - MASK_OFFSET + l;
 
                     // Range checks if hanging off the matrix
-                    if (offset_r >= 0 && offset_r < no_elements) {
-                        if (offset_c >= 0 && offset_c < no_elements) {
-                            // Accumulate partial results
-                            temp += m[offset_r * no_elements + offset_c] * mask[k * MASK_DIM + l];
+                    if (offset_r >= 0 && offset_r < no_elements)
+                    {
+                        if (offset_c >= 0 && offset_c < no_elements)
+                        {
+                            // Accumulate results into convo_output
+                            convo_output += main_vector[offset_r * no_elements + offset_c] * mask[k * MASK_DIM + l];
                         }
                     }
                 }
             }
+        }
+    }
+}
+
+// Initialises matrix
+// Arguments:
+//  main_vector = Matrix
+//  no_elements = Dimension of the matrix (square)
+void init_matrix(vector<int> main_vector, int no_elements)
+{
+    for (int i = 0; i < no_elements; i++)
+    {
+        for (int j = 0; j < no_elements; j++)
+        {
+            main_vector[no_elements * i + j] = rand() % 100;
         }
     }
 }
