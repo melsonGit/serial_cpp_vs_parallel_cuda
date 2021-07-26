9bc1772481efff3ac78ee0189062c3cfc53a1bad
melsonGit <50531920+melsonGit@users.noreply.github.com>
2021-07-26 20:55:50 +0100

    Update vector_addition_serial_final.cpp

--- final_project_code/serial/vector_addition_serial_final/vector_addition_serial_final/vector_addition_serial_final.cpp
+++ final_project_code/serial/vector_addition_serial_final/vector_addition_serial_final/vector_addition_serial_final.cpp
@@ -2,7 +2,7 @@
 //
 // Code sourced and adpated from the following author/s and sources: 
 // - https://solarianprogrammer.com/2012/04/11/vector-addition-benchmark-c-cpp-fortran/
-// - https://github.com/CoffeeBeforeArch/cuda_programming/blob/master/vectorAdd/baseline/vectorAdd.cu
+// - https://github.com/CoffeeBeforeArch/cuda_programming/blob/6589c89a78dee44e14ccb362cdae69f2e6850a2c/vectorAdd/baseline/vectorAdd.cu
 // - https://thispointer.com/how-to-fill-a-vector-with-random-numbers-in-c/
 // Please refer to the bibliography for a complete reference of the above author/s and sources
 
@@ -17,35 +17,36 @@ using std::vector;
 
 // Function Prototypes
 int element_set(int);
-void add(const vector<int>, const vector<int>, const vector<int>);
+void add(vector<int>, vector<int>, vector<int>);
 
 int main()
 {
-	// Call element_set function to assign variable no_elements with a user selected value
-	static int no_elements = element_set(no_elements);
+    // Call element_set function to assign variable no_elements with a user selected value
+    static int no_elements = element_set(no_elements);
 
-	vector<int> a(no_elements), b(no_elements), c(no_elements);
+    vector<int> a(no_elements), b(no_elements), c(no_elements);
 
     clock_t start = clock();
 
-	// Generate random numbers via Lambda C++11 function, and place into vector
-	generate(a.begin(), a.end(), []() { return rand() % 100; });
-	generate(b.begin(), b.end(), []() { return rand() % 100; });
+    // Generate random numbers via Lambda C++11 function, and place into vector
+    generate(a.begin(), a.end(), []() { return rand() % 100; });
+    generate(b.begin(), b.end(), []() { return rand() % 100; });
 
-	add(a, b, c);
+    add(a, b, c);
 
-	clock_t end = clock();
+    clock_t end = clock();
 
-	double diffs = (end - start) / (double)CLOCKS_PER_SEC;
-	cout << diffs << "s Vector Addition computation time, with an element size of " << no_elements << ".\n";
-	cout << "SEQUENTIAL VECTOR ADDITION COMPUTATION SUCCESSFUL.\nShutting down program....\n";
+    double diffs = (end - start) / (double)CLOCKS_PER_SEC;
+    cout << diffs << "s Vector Addition computation time, with an element size of " << no_elements << ".\n";
+    cout << "SEQUENTIAL VECTOR ADDITION COMPUTATION SUCCESSFUL.\nShutting down program....\n";
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 
 }
 
 // Function Declarations
-int element_set(int element_size) {
+int element_set(int element_size)
+{
 
     int temp_input;
 
@@ -62,27 +63,35 @@ int element_set(int element_size) {
         cout << "\n\nNo correct option selected!\nShutting down program....\n";
         return EXIT_FAILURE;
     }
-      // 25 million elements
-    if (temp_input == 1) {
+    // 25 million elements
+    if (temp_input == 1)
+    {
         element_size = 25000000;
     } // 35 million elements
-    else if (temp_input == 2) {
+    else if (temp_input == 2)
+    {
         element_size = 35000000;
     } // 45 million elements
-    else if (temp_input == 3) {
+    else if (temp_input == 3)
+    {
         element_size = 45000000;
     } // 55 million elements
-    else if (temp_input == 4) {
+    else if (temp_input == 4)
+    {
         element_size = 55000000;
     } // 65 million elements
-    else if (temp_input == 5) {
+    else if (temp_input == 5)
+    {
         element_size = 65000000;
     }
 
     return element_size;
 }
-void add(const vector<int> a, const vector<int> b, vector<int> c) {
-	// Add contents from vector 'a' and 'b' into vector 'c' || Transform using a Lambda C++11
-	transform(a.begin(), a.end(), b.begin(), c.begin(), 
-		[](int a, int b) {return a + b; });
+void add(vector<int> a, vector<int> b, vector<int> c)
+{
+    // Add contents from vector 'a' and 'b' into vector 'c' || Transform using a Lambda C++11
+    transform(a.begin(), a.end(), b.begin(), c.begin(),
+
+        [](int a, int b) { return a + b; });
 }
+
