0c8b1d20c259a572e2a0524c82a77dffa3b0ae41
melsonGit <50531920+melsonGit@users.noreply.github.com>
2021-11-30 22:18:31 +0000

    Add files via upload

--- final_project_code/parallel/vector_addition_parallel_final/vector_addition_parallel_final/kernel.cu
+++ final_project_code/parallel/vector_addition_parallel_final/vector_addition_parallel_final/kernel.cu
@@ -1,11 +1,4 @@
-// Parallel Vector Addition Program
-//
-// Code sourced and adpated from the following author/s and sources: 
-// - https://solarianprogrammer.com/2012/04/11/vector-addition-benchmark-c-cpp-fortran/
-// - https://github.com/CoffeeBeforeArch/cuda_programming/blob/6589c89a78dee44e14ccb362cdae69f2e6850a2c/vectorAdd/baseline/vectorAdd.cu
-// - https://www.youtube.com/watch?v=QVVTsLmMlwk
-// - https://thispointer.com/how-to-fill-a-vector-with-random-numbers-in-c/
-// Please refer to the bibliography for a complete reference of the above author/s and sources
+﻿// Parallel Vector Addition Program
 
 #include <algorithm>
 #include <iostream>
@@ -21,8 +14,7 @@ int element_set(int);
 
 // CUDA kernel for vector addition || Function that computes the sum of two vectors
 __global__ void vectorAdd(const int* __restrict a, const int* __restrict b,
-    int* __restrict c, int no_elements)
-{
+    int* __restrict c, int no_elements) {
     // Calculate global thread ID
     int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
 
@@ -31,24 +23,23 @@ __global__ void vectorAdd(const int* __restrict a, const int* __restrict b,
 
 }
 
-int main()
-{
+int main() {
 
     // Call element_set function to assign variable no_elements with a user selected value
     static int no_elements = element_set(no_elements);
     size_t bytes = sizeof(int) * no_elements;
 
     // Vectors for holding the host-side (CPU-side) data
-    vector<int> a(no_elements);
-    vector<int> b(no_elements);
-    vector<int> c(no_elements);
+    vector<int> a (no_elements);
+    vector<int> b (no_elements);
+    vector<int> c (no_elements);
 
     // Start the clock
     clock_t start = clock();
 
     // Initialise vector by generating random numbers via Lambda C++11 function
-    generate(a.begin(), a.end(), []() { return rand() % 100; });
-    generate(b.begin(), b.end(), []() { return rand() % 100; });
+    generate(a.begin(), a.end(), []() {return rand() % 100;});
+    generate(b.begin(), b.end(), []() {return rand() % 100;});
 
     // Allocate memory on the device (GPU)
     int* d_a, * d_b, * d_c;
@@ -67,7 +58,7 @@ int main()
     int NUM_BLOCKS = (no_elements + NUM_THREADS - 1) / NUM_THREADS;
 
     // Launch the kernel on the GPU
-    vectorAdd << < NUM_BLOCKS, NUM_THREADS >> > (d_a, d_b, d_c, no_elements);
+    vectorAdd << <NUM_BLOCKS, NUM_THREADS >> > (d_a, d_b, d_c, no_elements);
 
     // Copy sum vector from device to host
     cudaMemcpy(c.data(), d_c, bytes, cudaMemcpyDeviceToHost);
@@ -76,7 +67,7 @@ int main()
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
-
+    
     clock_t end = clock();
 
     double diffs = (end - start) / (double)CLOCKS_PER_SEC;
@@ -87,8 +78,7 @@ int main()
 }
 
 // Function Declarations
-int element_set(int element_size)
-{
+int element_set(int element_size) {
 
     int temp_input;
 
@@ -105,25 +95,20 @@ int element_set(int element_size)
         cout << "\n\nNo correct option selected!\nShutting down program....\n";
         return EXIT_FAILURE;
     }
-    // 25 million elements
-    if (temp_input == 1)
-    {
+        // 25 million elements
+    if (temp_input == 1) {
         element_size = 25000000;
     }   // 35 million elements
-    else if (temp_input == 2)
-    {
+    else if (temp_input == 2) {
         element_size = 35000000;
     }   // 45 million elements
-    else if (temp_input == 3)
-    {
+    else if (temp_input == 3) {
         element_size = 45000000;
     }   // 55 million elements
-    else if (temp_input == 4)
-    {
+    else if (temp_input == 4) {
         element_size = 55000000;
     }   // 65 million elements
-    else if (temp_input == 5)
-    {
+    else if (temp_input == 5) {
         element_size = 65000000;
     }
 
