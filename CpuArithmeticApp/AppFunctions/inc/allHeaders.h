1fbef97a6bcdddfbe3dddd1387ffbf9da2eb7099
MechLordJohn <50531920+MechLordJohn@users.noreply.github.com>
2021-12-07 22:32:28 +0000

    Implemented basic app file structure
    Added all folders I believe are needed for the current implementation plans. Header file allHeaders.h updated with new file paths with additional comments for future operation implementations.

--- CpuArithmeticApp/AppFunctions/inc/allHeaders.h
+++ CpuArithmeticApp/AppFunctions/inc/allHeaders.h
@@ -1,6 +1,28 @@
 #pragma once
 
-#include "addFunc.h"
-#include "checkAdd.h"
-#include "conSet.h"
-#include "numGen.h"
+// Core Function
+#include "core/opChoice.h"
+
+// Sequential Vector Addition
+#include "vecAdd/addFunc.h" // rename addFuncVec
+#include "vecAdd/checkAdd.h" // rename checkOpVec
+#include "vecAdd/conSet.h" // rename conSetVec
+#include "vecAdd/numGen.h" // rename numGenVec
+
+// Sequential Matrix Multiplication
+// #----- conSetMul
+// #----- numGenMul
+// #----- conSetMul
+// #----- numGenMul
+
+// Sequential 1-D Convolution
+// #----- conSetCon
+// #----- numGenCon
+// #----- conSetCon
+// #----- numGenCon
+
+// Sequential 2-D Convolution
+// #----- conSetTCon
+// #----- numGenTCon
+// #----- conSetTCon
+// #----- numGenTCon
--- CpuArithmeticApp/AppFunctions/inc/checkAdd.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <vector>
-
-void checkAdd(std::vector<int> const&, std::vector<int> const&, std::vector<int> const&);
