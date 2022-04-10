dbf57b1a4b668f3ebd3b2337cd0f007507a2c10a
melsonGit <50531920+melsonGit@users.noreply.github.com>
2022-04-10 16:15:10 +0100

    Implemented VectorAddition operation

--- CpuArithmeticApp/CpuArithmeticApp/src/VectorAddition.cpp
+++ CpuArithmeticApp/CpuArithmeticApp/src/VectorAddition.cpp
@@ -1,7 +1,11 @@
 #include "../inc/VectorAddition.h"
 
+#include <algorithm>
+#include <cassert>
+#include <iostream>
 #include <vector>
 
+// Worth defining this in ArithmeticOperations???
 void VectorAddition::startOpSeq(const int& userInput)
 {
 	setContainer(userInput);
@@ -18,9 +22,7 @@ void VectorAddition::setContainer(const int& userInput)
 	case 3: { this->mVAInputVecA.resize(mSampleSizes[2]); this->mVAInputVecB.resize(mSampleSizes[2]); this->mVAOutputVec.resize(mSampleSizes[2]); break; }
 	case 4: { this->mVAInputVecA.resize(mSampleSizes[3]); this->mVAInputVecB.resize(mSampleSizes[3]); this->mVAOutputVec.resize(mSampleSizes[3]); break; }
 	case 5: { this->mVAInputVecA.resize(mSampleSizes[4]); this->mVAInputVecB.resize(mSampleSizes[4]); this->mVAOutputVec.resize(mSampleSizes[4]); break; }
-	default:
-		// Something needs to go here....
-		break;
+	default: break; // Something needs to go here....
 	}
 
 	populateContainer(this->mVAInputVecA, this->mVAInputVecB);
@@ -28,8 +30,38 @@ void VectorAddition::setContainer(const int& userInput)
 
 void VectorAddition::launchOp()
 {
+	// Reminder: These event outputs could be placed into a class somewhere.......
+	std::cout << "\nVector Addition: Populating complete.\n";
+	std::cout << "\nVector Addition: Starting operation.\n";
+
+	// Add contents from inputVecA and inputVecB into resultVec
+	transform(this->mVAInputVecA.begin(), this->mVAInputVecA.end(), this->mVAInputVecB.begin(), this->mVAOutputVec.begin(),
+		[](auto a, auto b) {return a + b; });
+
+	std::cout << "\nVector Addition: Operation complete.\n";
 }
 
 void VectorAddition::validateResults() 
 {
+	std::cout << "\nVector Addition: Authenticating results.\n\n";
+
+	// Determines result authenticity - Assigned false value when results don't match
+	bool doesMatch{ true };
+
+	// For each row in inputVecA/B 
+	for (auto rowId{ 0 }; rowId < this->mVAOutputVec.size() && doesMatch; ++rowId)
+	{
+		// Check addition of both rows matches value in corresponding row in resultVec
+		if ((this->mVAInputVecA[rowId] + this->mVAInputVecB[rowId]) != this->mVAOutputVec[rowId])
+			doesMatch = false;
+	}
+	// Assert and abort when results don't match
+	assert(doesMatch && "Check failed! Addition of mVAInputVecA / mVAInputVecB values don't match corresponding values in mVAOutputVec (vecAdd).");
+
+	if (!doesMatch)
+		std::cerr << "Vector Addition unsuccessful: output vector data does not match expected results.\n"
+		<< "Timing results will be discarded.\n\n";
+	else
+		std::cout << "Vector Addition successful: output vector data matches expected results.\n"
+		<< "Timing results will be recorded.\n\n";
 }
