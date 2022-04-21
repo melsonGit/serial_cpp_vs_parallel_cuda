e8257af706a4b32e6743b0ca8afeaca3bddf18dd
melsonGit <50531920+melsonGit@users.noreply.github.com>
2022-04-21 20:02:11 +0100

    Minor update to OperationTimer structure

--- CpuArithmeticApp/inc/OperationTimer.h
+++ CpuArithmeticApp/inc/OperationTimer.h
@@ -1,8 +1,19 @@
+#include <chrono>
+
+using Clock = std::chrono::steady_clock;
 
 class OperationTimer
 {
 private:
 
+
+	// Start clock
+	auto opStart{ Clock::now() };
+	// Stop clock
+	auto opEnd{ Clock::now() };
+
 public:
 
+	std::chrono::duration_cast<std::chrono::microseconds>(opEnd - opStart).count() << " us\n"
+		<< std::chrono::duration_cast<std::chrono::milliseconds>(opEnd - opStart).count() <<
 };
